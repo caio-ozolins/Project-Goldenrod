8c0fed5927ce59c679366c9a92f3a83a7cee45c7
Filipe Eduardo <filipeeacosta@gmail.com>
2024-05-21 00:57:29 -0300

    Scene[Level02]: Water mine
    Ignore the THING
    (Foi krl!!!)
    
    Co-Authored-By: Caio Ozolins Mendes <105940905+caio-ozolins@users.noreply.github.com>

--- Assets/Scripts/PlayerDig.cs
+++ Assets/Scripts/PlayerDig.cs
@@ -8,48 +8,62 @@ public class PlayerDig : MonoBehaviour
     private GameObject ironOre;
     private GameObject diamondOre;
     private GameObject coalOre;
+    private GameObject waterBefore;
+
+    private GameObject atrocidade;
+    private GameObject atrocidadeJr;
 
     private GameObject placaFerrar;
     private GameObject placaDima;
     private GameObject placaCoal;
+    private GameObject placaWater;
 
     private Sign signIron;
     private Sign signDima;
     private Sign signCoal;
+    private Sign signWater;
 
     public GameObject blackScreen;
     public GameObject avisoFerro;
     public GameObject avisoDima;
     public GameObject avisoCoal;
+    public GameObject avisoWater;
 
     public Audiomanager audioManager;
     private PlayerMovement player;
     private String tagPlaca;
+    private GameObject waterSpawner;
 
     private void Start()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<Audiomanager>();
+        waterSpawner = GameObject.FindGameObjectWithTag("WaterSpawner");
+
+        atrocidade = GameObject.FindGameObjectWithTag("Atrocidade");
+        atrocidadeJr = GameObject.FindGameObjectWithTag("AtrocidadeJr");
 
         ironOre = GameObject.FindGameObjectWithTag("IronOre");
         diamondOre = GameObject.FindGameObjectWithTag("DiamondOre");
         coalOre = GameObject.FindGameObjectWithTag("CoalOre");
+        waterBefore = GameObject.FindGameObjectWithTag("WaterBefore");
 
         placaFerrar = GameObject.FindGameObjectWithTag("PlacaFerrar");
         placaDima = GameObject.FindGameObjectWithTag("PlacaDima");
         placaCoal = GameObject.FindGameObjectWithTag("PlacaCoal");
+        placaWater = GameObject.FindGameObjectWithTag("PlacaWater");
 
         signIron = GameObject.FindGameObjectWithTag("PlacaFerrar").GetComponent<Sign>();
         signDima = GameObject.FindGameObjectWithTag("PlacaDima").GetComponent<Sign>();
         signCoal = GameObject.FindGameObjectWithTag("PlacaCoal").GetComponent<Sign>();
+        signWater = GameObject.FindGameObjectWithTag("PlacaWater").GetComponent<Sign>();
+
         player = FindObjectOfType<PlayerMovement>();
     }
 
     public void Dig(InputAction.CallbackContext context)
     {
-        Debug.Log("Fora");
-        if (signIron.nearSign || signDima.nearSign || signCoal.nearSign && context.performed)
+        if (signIron.nearSign || signDima.nearSign || signCoal.nearSign || signWater.nearSign && context.performed)
         {
-            Debug.Log("Dinicio");
             Time.timeScale = 0;
             blackScreen.SetActive(true);
 
@@ -73,12 +87,21 @@ public class PlayerDig : MonoBehaviour
             }
             else if (tagPlaca == "PlacaCoal")
             {
-                Debug.Log("carvao");
                 Destroy(coalOre);
                 Destroy(placaCoal);
                 PlayerPrefs.SetInt("CoalOre", 1);
                 StartCoroutine(WaitDig());
                 avisoCoal.SetActive(true);
+            }else if (tagPlaca == "PlacaWater")
+            {
+                Destroy(waterBefore);
+                Destroy(placaWater);
+                Destroy(waterSpawner);
+                Destroy(atrocidade);
+                Destroy(atrocidadeJr);
+                PlayerPrefs.SetInt("Water", 1);
+                StartCoroutine(WaitDig());
+                avisoWater.SetActive(true);
             }
         }
     }
@@ -99,6 +122,7 @@ public class PlayerDig : MonoBehaviour
         avisoFerro.SetActive(false);
         avisoDima.SetActive(false);
         avisoCoal.SetActive(false);
+        avisoWater.SetActive(false);
         blackScreen.SetActive(false);
         Time.timeScale = 1;
     }
