23948078a2fa86217fd480afedc144d0be0c5d64
Filipe Eduardo <filipeeacosta@gmail.com>
2024-06-24 20:28:27 -0300

    Scene[Level01]: Barrier
    Co-Authored-By: Caio Ozolins Mendes <105940905+caio-ozolins@users.noreply.github.com>
    Co-Authored-By: Gu0704 <100092810+Gu0704@users.noreply.github.com>
    Co-Authored-By: VitoBruno <106201232+VitoBruno@users.noreply.github.com>

---
+++ Assets/Scripts/Barrier.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    [SerializeField] private GameObject warningUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && PlayerPrefs.GetInt("FixedDrill") == 0)
+        {
+            warningUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            warningUI.SetActive(false);
+        }
+    }
+}
